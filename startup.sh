a270ecfa2723926df619b7442e78fd2772fbd575
oscar jhosue pastor quispe <73138388@continental.edu.pe>
2026-06-25 10:38:38 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo "Instalando ODBC..."
+echo "Iniciando FastAPI..."
 
-apt-get update
-apt-get install -y curl gnupg2
-
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
-
-echo "Iniciando Gunicorn..."
-gunicorn --bind=0.0.0.0:8000 --workers 2 app:app
+uvicorn app:app --host=0.0.0.0 --port=8000 --workers 2
